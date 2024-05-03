baf58e90a4cfbf9d0c63b562773c1239971b4fc1
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-03 16:31:25 -0700

    Set up devise, generate user controller, and add user authentication to application controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Set up config for Devise mailer in development
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
