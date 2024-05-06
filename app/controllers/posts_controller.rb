89c5d4842476a5e2b73ce4a9f7e04914328e5635
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-06 15:41:12 -0700

    Add routes and fill out prelim post controller and post#index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      redirect_to new_post_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
+  resources :comment
+  resources :like
+  resources :post
+  resources :user
 end
