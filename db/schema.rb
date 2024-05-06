a6e0696434ab6f1591ed8b0c5350955e16efe565
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-06 14:07:48 -0700

    Add like model and likes controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_06_210614) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_210720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
