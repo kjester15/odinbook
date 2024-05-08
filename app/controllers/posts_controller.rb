dc912deb15a101d64a8df092832e477b1590a0fe
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-07 17:05:53 -0700

    Add delete post button to posts, create posts#destroy, and add dependent destroy to applicable associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,26 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
