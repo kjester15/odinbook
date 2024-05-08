566cf2474c5f57222eb0a8f54bf41d11a9a7ac49
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-08 15:06:03 -0700

    Move comment form to partial and add delete comment button and comments#destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def comment_params
