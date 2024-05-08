fcded19829edaac9171c10d4b8b98e81ea538053
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-08 14:27:23 -0700

    Add placeholder text to comment text area and add background color to post-container

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to posts_path
+    else
+      redirect_to posts_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id)
+  end
 end
