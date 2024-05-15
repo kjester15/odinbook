6ad92b0042006890d8a2fea0595e144f2dcce9b3
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-14 17:53:22 -0700

    Build prelim follow new and create methods in controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,21 @@
 class FollowsController < ApplicationController
+  def new
+    @follow = Follow.new
+  end
+
+  def create
+    @follow = current_user.followers.build(follow_params)
+
+    if @follow.save
+      redirect_to users_path
+    else
+      redirect_to users_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def follow_params
+    params.require(:follow).permit(:follower_id, :followee_id)
+  end
 end
