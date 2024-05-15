7e988cd48bddb5862583af571a1b2d8ed290b5c0
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-14 17:05:59 -0700

    Add user creation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# User.create = ([username: "user1", email: "user1@email.com", password: "user1pw"])
+# User.create = ([username: "user2", email: "user2@email.com", password: "user2pw"])
+# User.create = ([username: "user3", email: "user3@email.com", password: "user3pw"])
+# User.create = ([username: "user4", email: "user4@email.com", password: "user4pw"])
+# User.create = ([username: "user5", email: "user5@email.com", password: "user5pw"])
+
+5.times do |i|
+  User.create([username: "user#{i}", email: "user#{i}@email.com", password: "user#{i}pw"])
+end
