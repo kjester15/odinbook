9ccff026b2d17ec9b773efd2f20d635435ee5cc7
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-21 16:27:48 -0700

    Get username from email address during google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,16 +17,14 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-
     user = User.where(email: data['email']).first
-
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
       user = User.create(name: data['name'],
                          email: data['email'],
                          password: Devise.friendly_token[0, 20],
-                         username: data['username'],
-                         image: data['image'])
+                         image: data['image'],
+                         username: data['email'].split('@')[0])
     end
     user
   end
