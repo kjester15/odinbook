2bc1d3e0e476e1bd2ac5c53dfda7ae8d73f449e2
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-29 14:54:35 -0700

    Add about page and update current user profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:index, :show]
-  # get 'profile', to: 'users#show'
+  get 'profile', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,10 +9,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "pages#index"
   resources :likes
   resources :follows
   resources :posts do
     resources :comments
   end
+  resources :pages, :path => "about", only: [:index]
 end
