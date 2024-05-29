919727bffa3760f77f57dc08526f0824256146f7
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-28 18:21:37 -0700

    Fix post delete issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to request.referrer
   end
 
   private
