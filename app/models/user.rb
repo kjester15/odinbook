573a6ef7bcc80c6c934edc691bb03ad0cbe6e951
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-28 18:00:01 -0700

    Style devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, 
+         :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
