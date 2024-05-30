91b35bbe0816f514b577921aba1fe2a8f2daafbc
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-05-30 16:32:04 -0700

    Add dependent destroy to user model for follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   # follower_follows "names" the Follow join table for accessing through the follower association
-  has_many :follower_follows, foreign_key: "followee_id", class_name: "Follow"
+  has_many :follower_follows, foreign_key: "followee_id", class_name: "Follow", dependent: :destroy
   # source: :follower matches with the belong_to :follower identification in the Follow model
-  has_many :followers, through: :follower_follows, source: :follower
+  has_many :followers, through: :follower_follows, source: :follower, dependent: :destroy
   # followee_follows "names" the Follow join table for accessing through the followee association
-  has_many :followee_follows, foreign_key: "follower_id", class_name: "Follow"
+  has_many :followee_follows, foreign_key: "follower_id", class_name: "Follow", dependent: :destroy
   # source: :followee matches with the belong_to :followee identification in the Follow model
-  has_many :followees, through: :followee_follows, source: :followee
+  has_many :followees, through: :followee_follows, source: :followee, dependent: :destroy
 
   def self.from_omniauth(access_token)
     data = access_token.info
